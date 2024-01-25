4f043127146a7800180a300a32fcd957f72679c0
NCP08 <gretad12@naver.com>
2024-01-25 17:16:22 +0900

    ForEx1.java 초기 조건 증가 한번에 넣고 돌리기

---
+++ Ch03_Control/src/ch03/for10/ForEx1.java
@@ -0,0 +1,13 @@
+package ch03.for10;
+
+public class ForEx1 {
+
+	public static void main(String[] args) {
+		for(int i=1; i<=10; i++) {			// 초기 & 조건 & 증가
+			System.out.println(i);	// 실행
+
+			
+		}
+
+	}
+}
