d58a49d2e654ddba67057523b5a3cd8813320b76
NCP08 <gretad12@naver.com>
2024-01-25 14:41:36 +0900

    If~else 문 예제

---
+++ Ch03_Control/src/ch03/control01/IfEx.java
@@ -0,0 +1,19 @@
+package ch03.control01;
+
+import java.util.Scanner;
+
+public class IfEx {
+	public static void main(String[] args) {
+		int score;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("0~100 intput");
+		score = sc.nextInt();
+		
+//		String Text = score >= 90 ? "A" : "B";
+		if(score >= 90) {
+			System.out.println("A");
+		}
+		
+	}
+}
---
+++ Ch03_Control/src/ch03/control02/IfEx.java
@@ -0,0 +1,21 @@
+package ch03.control02;
+
+import java.util.Scanner;
+
+public class IfEx {
+	public static void main(String[] args) {
+		int score;
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("0~100 intput");
+		score = sc.nextInt();
+		
+//		String Text = score >= 90 ? "A" : "B";
+		if(score >= 90) {
+			System.out.println("true A");
+		}else {
+			System.out.println("Not A");
+		}
+		
+	}
+}
