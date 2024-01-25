3fd612e050088f7b4ebba26fd5bca1ecb89b6b74
NCP08 <gretad12@naver.com>
2024-01-25 17:08:24 +0900

    forEx.java

---
+++ Ch03_Control/src/ch03/for10/ForEx.java
@@ -0,0 +1,14 @@
+package ch03.for10;
+
+public class ForEx {
+
+	public static void main(String[] args) {
+		int i = 1;					// 초기
+		for(;i<=10;i++) {			// 조건 & 증가
+			System.out.println(i);	// 실행
+			
+		}
+
+	}
+
+}
