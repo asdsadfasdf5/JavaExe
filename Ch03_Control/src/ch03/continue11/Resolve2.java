be903033d6f28eaaa08de5431f45960a15cf4e62
NCP08 <gretad12@naver.com>
2024-01-26 14:15:52 +0900

    문제3개

--- Ch03_Control/src/ch03/continue11/Resolve2.java
+++ Ch03_Control/src/ch03/continue11/Resolve2.java
@@ -3,12 +3,14 @@ package ch03.continue11;
 public class Resolve2 {
 
 	public static void main(String[] args) {
-//	for문(2중X). 별 쌓기. 1~5개까지
-//		ver_one();
-		ver_two();
+		// for문(2중X). 별 쌓기. 1~5개까지
+		// 별을 하나 더 찍기 위해서는 +=와같은 내용이 필요
+		// i가 ++될때 *도 같이 늘려주는 작업이 필요
+		ver_one(); // version1
+		ver_two(); // version2
 	}
 
-	public static void ver_0ne() {
+	public static void ver_one() {
 		for (int i = 1; i <= 5; i++) {
 			String star = "*";
 			if (i == 1) {
@@ -32,8 +34,12 @@ public class Resolve2 {
 	}
 
 	public static void ver_two() {
+
+		String star = "";
 		for (int i = 1; i <= 5; i++) {
-			
+			star += "*";
+			System.out.println(star);
+
 		}
 	}
 
