b809dfde12f8f66c0b647b62d888daab576f0bb4
NCP08 <gretad12@naver.com>
2024-01-29 15:44:04 +0900

    method 분리 예제

--- Ch06_method/src/ch06/method01/MethodEx4.java
+++ Ch06_method/src/ch06/method01/MethodEx4.java
@@ -2,12 +2,12 @@ package ch06.method01;
 
 import java.util.Scanner;
 
-public class MethodEx2 {
-
-	public static int calcArith(int num0, int num1, String op) {
+public class MethodEx4 {
+	
+	public static void add(int num0, int num1,String op) {
 		int result = 0;
 		// 계산
-		switch (op) {
+		switch(op) {
 		case "+" -> result = num0 + num1;
 		case "-" -> result = num0 - num1;
 		case "*" -> result = num0 * num1;
@@ -15,29 +15,25 @@ public class MethodEx2 {
 		case "%" -> result = num0 % num1;
 		default -> System.out.println("연산이 불가능 합니다.");
 		}
-		return result;
-	}
-
-	public static void viewResult(int result) {
 		// 결과 출력
-		System.out.println("결과값은 " + result + "입니다.");
-	}
+		System.out.println("결과값은 "+ result + "입니다.");
 
+	}
 //	메서드를 만들기 전 코드
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
-
+		
 		// 입력
 		System.out.println("1번째 정수 입력 >>");
-		int num0 = sc.nextInt();
+		int num0 = sc.nextInt();		
 		System.out.println("2번째 정수 입력 >>");
-		int num1 = sc.nextInt();
+		int num1 = sc.nextInt();		
 		System.out.println("연산자 입력(+,-,*,/,%) >>");
-		String op = sc.next();
+		String op = sc.next();		
+
+		add(num0,num1,op);
 		
-		int result = calcArith(num0, num1, op);
-		viewResult(result);
 		
 		sc.close();
 	}
